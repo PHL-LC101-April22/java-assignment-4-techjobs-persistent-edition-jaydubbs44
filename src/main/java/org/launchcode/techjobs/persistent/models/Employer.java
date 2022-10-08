6d82eaa962f7f9f79c45b05b0321b2ce1a52bf6d
Will Christen <christenwill58@gmail.com>
2022-10-08 17:28:12 -0400

    All tests passing in TestTaskTwo.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "You must specify a location")
+    @NotNull(message = "You must specify a location")
+    @Size(min=1, max=60, message = "The name of the location must be between 1 and 60 characters long.")
+    private String location;
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
